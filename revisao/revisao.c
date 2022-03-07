247f2acaaab8b64d8ab4aa489d82ce8845b50f29
Vinícius Guasso <viniguasso10@gmail.com>
2022-03-07 11:25:42 -0300

    revisao

---
+++ revisao/revisao.c
@@ -0,0 +1,32 @@
+// Construir um programa que receba nome, altura de um possível atleta.
+#include <stdio.h>
+#define TAM 2
+
+typedef struct{
+	char nome[10];
+	float altura, peso;
+}atleta;
+
+char *escolhe(atleta a);
+
+int main(void) {
+	atleta umatleta;
+	printf("\nQual seu nome? ");
+	scanf("%s", umatleta.nome);
+	printf("\nQual o seu peso em KG? ");
+	scanf("%f", &umatleta.peso);
+	printf("\nQual a sua altura em cm? ");
+	scanf("%f", &umatleta.altura);
+	char *resposta = escolhe(umatleta);
+	printf("\nEsporte: %s", resposta);
+}
+
+char *escolhe(atleta a) {
+	if (a.altura<180 && a.peso<70) {
+		return "Atletismo";
+	}
+	else if (a.altura>180 && a.peso<70) {
+		return "Volei ou BVasquete";
+	}
+	return "Futebol";
+}
