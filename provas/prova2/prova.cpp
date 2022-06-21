7f4bf9ed7c27e810916a82f8b4db35b9ee259df0
Vinícius Guasso <83726401+viniguasso@users.noreply.github.com>
2022-06-21 08:30:47 -0300

    identação

--- provas/prova2/prova.cpp
+++ provas/prova2/prova.cpp
@@ -6,31 +6,31 @@ using namespace std;
 #include "arvores.h"
 
 int main() {
-    // 1)
-	ifstream procurador;
-    char nomeArquivo[200];
-    cout << "Nome do arquivo fonte com os numeros: ";
-    cin >> nomeArquivo;
+	// 1)
+    	ifstream procurador;
+	char nomeArquivo[200];
+	cout << "Nome do arquivo fonte com os numeros: ";
+	cin >> nomeArquivo;
 
-    procurador.open(nomeArquivo);
-    if (!procurador) {
-        cout << "Arquivo nao localizado!\n";
-        exit(0);
-    }
+	procurador.open(nomeArquivo);
+	if (!procurador) {
+		cout << "Arquivo nao localizado!\n";
+		exit(0);
+	}
 
-    int valor;
-    Arvore *raiz = NULL;
-    do {
-        procurador >> valor;
-        raiz = inserir(valor, raiz);
-    } while(!procurador.eof());
-    procurador.close();
-    exibir(raiz,0);
+	int valor;
+	Arvore *raiz = NULL;
+	do {
+		procurador >> valor;
+		raiz = inserir(valor, raiz);
+	} while(!procurador.eof());
+	procurador.close();
+	exibir(raiz,0);
 
 	// 2)
-    int menor = menorElemento(raiz)->valor;
+    	int menor = menorElemento(raiz)->valor;
 	int menorNivel = localizarNivel(menor, raiz);
-    int maior = maiorElemento(raiz)->valor;
+    	int maior = maiorElemento(raiz)->valor;
 	int maiorNivel = localizarNivel(maior, raiz);
 	
 	if (maiorNivel == menorNivel) {
@@ -41,5 +41,5 @@ int main() {
 
 	// 3)
 	cout << "Eh folha: " << verificarFolha(25, raiz) << endl;
-    return 1;
+    	return 1;
 }
